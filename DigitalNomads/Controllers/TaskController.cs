e173457d87f5e6f3b818a64f94593f3bb882ec17
scelosmano1 <scelosmano1@etf.unsa.ba>
2020-11-20 22:53:02 +0100

    dobavljeni podaci iz baze i uspjesan prikaz na view-u

---
+++ DigitalNomads/Controllers/TaskController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DigitalNomads.Data;
+using DigitalNomads.Models.Task;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
+using DigitalNomads.Models.Account;
+
+namespace DigitalNomads.Controllers
+{
+    public class TaskController : Controller
+    {
+        private CtrlAltDefeatDbContext _dbContext;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+  
+        public TaskController(CtrlAltDefeatDbContext dbContext, IHttpContextAccessor httpContextAccessor)
+        {
+            this._dbContext = dbContext;
+            this._httpContextAccessor = httpContextAccessor;
+        }
+
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public async Task<IActionResult> AllTasks()
+        {
+            TaskList list = await GetAllTasksByIdAsync();
+
+            return View(list);
+        }
+
+        public async Task<int> GetAccountIdByUserIdAsync(string UserId)
+        {
+            AccountGetDetailsRes res = await _dbContext.Accounts
+                .Where(a => a.UserId == UserId)
+                .Select(a => new AccountGetDetailsRes
+                {
+                    Id = a.Id,
+                    FirstName = a.FirstName,
+                    LastName = a.LastName,
+                    Lat = a.Lat,
+                    Long = a.Long
+
+                }).FirstOrDefaultAsync();
+
+            return res.Id;
+        }
+
+        public async Task<TaskList> GetAllTasksByIdAsync ()
+        {
+            var userId = _httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
+            int accId = await GetAccountIdByUserIdAsync(userId);
+
+            IList<TaskRes> res = await _dbContext.Tasks
+                .Where(t => t.UserId == accId)
+                .Select(t => new TaskRes
+                {
+                    Id = t.Id,
+                    Title = t.Title,
+                    IsFinished = t.IsFinished,
+                    Deadline = t.Deadline,
+                    Description = t.Description,
+                    UserId = t.UserId
+                }).ToListAsync();
+
+            return new TaskList { Tasks = res };
+        }
+
+
+    }
+}
--- DigitalNomads/Startup.cs
+++ DigitalNomads/Startup.cs
@@ -35,6 +35,8 @@ namespace DigitalNomads
                 .AddEntityFrameworkStores<CtrlAltDefeatDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
+
+            services.AddScoped<CtrlAltDefeatDbContext, CtrlAltDefeatDbContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
