7663193892f48281359149c80c11cbd02b0b1272
Elma Škulj <elmaskulj@users.noreply.github.com>
2020-11-20 19:15:26 +0100

    napravljena baza

--- DigitalNomads/Entities/Account.cs
+++ DigitalNomads/Entities/Account.cs
@@ -8,6 +8,7 @@ namespace DigitalNomads.Entities
 {
     public class Account
     {
+        public int Id { get; set; }
         public string UserId { get; set; }
         public virtual IdentityUser User { get; set; }
         public string FirstName { get; set; }
