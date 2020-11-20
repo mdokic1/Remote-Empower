bb4202b62ce7b423fb02f32c25557fde43ba3a45
kdokic1 <kdokic1@etf.unsa.ba>
2020-11-20 18:41:39 +0100

    dodani entiteti

---
+++ DigitalNomads/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DigitalNomads.Entities
+{
+    public class User : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public double Lat { get; set; }
+        public double Long { get; set; }
+        public int TeamId { get; set; }
+        public Team Team { get; set; }
+
+    }
+}
